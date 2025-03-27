a5678ef55c7fb5bd2b8c0ee701081e6c43bc23e0
Ningwahang2003 <ningwahang06@gmail.com>
2025-03-27 22:51:28 +0545

    commited

--- obj/Debug/net8.0/GuideMe.AssemblyInfo.cs
+++ obj/Debug/net8.0/GuideMe.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GuideMe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93a12fc2913a1b1dc8ba841824c9e1574a3361ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72edcae0db6207b44083923506c6527118c0ef45")]
 [assembly: System.Reflection.AssemblyProductAttribute("GuideMe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GuideMe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
